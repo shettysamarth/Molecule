bfe5115879035cc6f39e17b56412d3971241cac1
Samarth Shetty <samarthrshetty@gmail.com>
2016-03-02 23:32:51 -0500

    added header definition

--- server.js
+++ server.js
@@ -135,6 +135,24 @@
          mongoose.connect(url);
 
 
+         self.app.use(function (req, res, next) {
+
+             // Website you wish to allow to connect
+             res.setHeader('Access-Control-Allow-Origin', "*");
+
+             // Request methods you wish to allow
+             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+             // Request headers you wish to allow
+             res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+             // Set to true if you need the website to include cookies in the requests sent
+             // to the API (e.g. in case you use sessions)
+             res.setHeader('Access-Control-Allow-Credentials', true);
+
+             // Pass to next layer of middleware
+             next();
+         });
          self.app.use(express.static(__dirname+'/public'));
          self.app.get('/', function(req, res){
              res.send('hello world');
@@ -198,6 +216,8 @@
                  getCourseOutcomeDetails(listOfOutcomes, function(courseOutcomeResult)
                  {
                      results[0].courseOutcomes = courseOutcomeResult;
+                     //res.header("Access-Control-Allow-Origin", "*");
+                     //res.header("Access-Control-Allow-Headers", "X-Requested-With");
                      res.json(results);
                  });
              });
