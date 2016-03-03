7f241bd75f283f02034c4e18cf5ebfad76e86a59
Samarth Shetty <samarthrshetty@gmail.com>
2016-03-02 23:36:12 -0500

    header

--- server.js
+++ server.js
@@ -143,7 +143,7 @@
              // Request methods you wish to allow
              res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-             // Request headers you wish to allow
+             // Request headers you wisgto allow
              res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
              // Set to true if you need the website to include cookies in the requests sent
